7444998ed9e4c9707c1c4e24d6e3ac12b0be5e3d
raihanmd <mraihanf471@gmail.com>
2024-11-10 17:16:36 +0700

    init

---
+++ src/server/HandleReceitionist.server.lua
@@ -0,0 +1,45 @@
+local noobert = workspace.Noobert
+local book = workspace.Receptionist["Open book"]
+local seat = workspace.Chair.Seat -- Reference to the Seat part in the chair
+local chairPrompt = seat:FindFirstChild("ProximityPrompt")
+local followDistance = 5
+local isFollowing = false
+local isSitting = false
+
+-- Follow the player
+book.ProximityPrompt.Triggered:Connect(function(plr)
+	local character = plr.Character
+	if character and not isFollowing and not isSitting then
+		isFollowing = true
+
+		game:GetService("RunService").Heartbeat:Connect(function()
+			if isFollowing and character then
+				local targetPosition = character.HumanoidRootPart.Position
+				local currentPosition = noobert.HumanoidRootPart.Position
+
+				if (currentPosition - targetPosition).Magnitude > followDistance then
+					noobert.Humanoid:MoveTo(targetPosition)
+				else
+					noobert.Humanoid:MoveTo(currentPosition) -- Stop moving
+				end
+			end
+		end)
+	end
+end)
+
+-- Move noobert to the seat and make him sit down
+chairPrompt.Triggered:Connect(function()
+	if not isSitting then
+		isFollowing = false
+		isSitting = true
+
+		-- Move noobert to the seat position
+		noobert.Humanoid:MoveTo(seat.Position)
+
+		-- Wait until noobert reaches the seat and sits down
+		noobert.Humanoid.MoveToFinished:Wait()
+
+		-- Ensure he sits by setting `Sit` to true
+		noobert.Humanoid.Sit = true
+	end
+end)
