f44de402abf02413730abedee7c547ae35a8f0b5
raihanmd <mraihanf471@gmail.com>
2025-03-02 11:42:08 +0700

    add CCTV handler for proximity prompt interaction

---
+++ src/StarterPlayer/StarterPlayerScripts/CCTVHandler.client.lua
@@ -0,0 +1,13 @@
+local CollectionService = game:GetService("CollectionService")
+
+local CCTVProximity = workspace:WaitForChild("Computer"):WaitForChild("Proximity").ProximityPrompt
+
+local camera = workspace.CurrentCamera
+
+CCTVProximity.Triggered:Connect(function(palyer: Player)
+	local cctv = CollectionService:GetTagged("CCTV_Prototype")
+
+	camera.CameraType = Enum.CameraType.Scriptable
+
+	camera.CFrame = cctv[1].CFrame
+end)
